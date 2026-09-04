618e518288cc1495835a547b5e5fea2359b6386d
IncharaDJ <incharanadeem@gmail.com>
2026-09-04 23:04:16 +0530

    App.jsx

---
+++ App.jsx
@@ -0,0 +1,21 @@
+```jsx
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="landing-page">
+      <h1>Paradise Nursery</h1>
+
+      <p>
+        Welcome to Paradise Nursery – your one-stop shop for beautiful
+        indoor and outdoor plants.
+      </p>
+
+      <button>Get Started</button>
+    </div>
+  );
+}
+
+export default App;
+```
